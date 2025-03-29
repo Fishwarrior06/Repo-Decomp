98aa95407a81ad2575624a4da007da8737c32d63
Fishwarrior06 <hectordavidortegagarcia@gmail.com>
2025-03-29 02:17:02 -0600

    MoreBuyableItems wuuuuu

--- REPO_Rebalanced/Plugin.cs
+++ REPO_Rebalanced/Plugin.cs
@@ -1,7 +1,10 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
+using BepInEx.Configuration;
 using HarmonyLib;
 using System.Reflection;
+using System.IO;
+using UnityEngine;
 
 namespace REPO_Rebalanced
 {
@@ -9,15 +12,54 @@ namespace REPO_Rebalanced
     public class Plugin : BaseUnityPlugin
     {
         internal static new ManualLogSource Logger;
+        internal new static ConfigFile Config;
+
+        private static readonly (string name, float defaultValue)[] ItemSpawnSettings =
+        {
+            ("Item Drone Battery", 0.3f),
+            ("Item Drone Feather", 0.3f),
+            ("Item Drone Indestructible", 0.3f),
+            ("Item Drone Torque", 0.3f),
+            ("Item Drone Zero Gravity", 0.5f),
+            ("Item Extraction Tracker", 0.5f),
+            ("Item Gun Handgun", 0.6f),
+            ("Item Gun Shotgun", 0.3f),
+            ("Item Gun Tranq", 0.5f),
+            ("Item Melee Baseball Bat", 0.7f),
+            ("Item Melee Frying Pan", 0.6f),
+            ("Item Melee Sledge Hammer", 0.5f),
+            ("Item Melee Sword", 0.5f)
+        };
+
+        public static System.Collections.Generic.List<ItemSpawnInfo> ItemSpawnInfos = new System.Collections.Generic.List<ItemSpawnInfo>();
 
         private void Awake()
         {
             Logger = base.Logger;
             Logger.LogInfo($"Plugin REPO Rebalanced is loaded!");
 
-            // Aplica todos los Harmony patches de este assembly
+            Config = new ConfigFile(Path.Combine(Paths.ConfigPath, "REPO_Rebalanced.cfg"), true);
+
+            foreach (var setting in ItemSpawnSettings)
+            {
+                var configEntry = Config.Bind("ItemSpawnProbabilities", setting.name, setting.defaultValue, $"Probabilidad de aparición para {setting.name}");
+                ItemSpawnInfos.Add(new ItemSpawnInfo(setting.name, configEntry.Value));
+            }
+
             Harmony harmony = new Harmony("com.fishyorch.rebalanced");
             harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    public class ItemSpawnInfo
+    {
+        public string ItemName { get; set; }
+        public float SpawnChance { get; set; }
+
+        public ItemSpawnInfo(string itemName, float spawnChance)
+        {
+            ItemName = itemName;
+            SpawnChance = spawnChance;
         }
     }
 }
--- REPO_Rebalanced/patches/MoreBuyableItems.cs
+++ REPO_Rebalanced/patches/MoreBuyableItems.cs
@@ -1,53 +1,21 @@
 using HarmonyLib;
-using System.Collections.Generic;
 using System.Linq;
 using System;
-using UnityEngine;
 
 namespace REPO_Rebalanced
 {
-    // Clase auxiliar para mantener la información del ítem y su probabilidad de aparición
-    public class ItemSpawnInfo
-    {
-        public string ItemName { get; set; }
-        public float SpawnChance { get; set; }
-
-        public ItemSpawnInfo(string itemName, float spawnChance)
-        {
-            ItemName = itemName;
-            SpawnChance = spawnChance;
-        }
-    }
-
     [HarmonyPatch(typeof(ShopManager), "GetAllItemsFromStatsManager")]
     public class ShopManagerPatch
     {
-        // Declarar la lista como estática para que sea accesible desde otras clases
-        public static List<ItemSpawnInfo> itemSpawnInfos = new List<ItemSpawnInfo>
-        {
-            new ItemSpawnInfo("Item Drone Battery", 0.3f),
-            new ItemSpawnInfo("Item Drone Feather", 0.3f),
-            new ItemSpawnInfo("Item Drone Indestructible", 0.3f),
-            new ItemSpawnInfo("Item Drone Torque", 0.3f),
-            new ItemSpawnInfo("Item Drone Zero Gravity", 0.5f),
-            new ItemSpawnInfo("Item Extraction Tracker", 0.5f),
-            new ItemSpawnInfo("Item Gun Handgun", 0.6f),
-            new ItemSpawnInfo("Item Gun Shotgun", 0.3f),
-            new ItemSpawnInfo("Item Gun Tranq", 0.5f),
-            new ItemSpawnInfo("Item Melee Baseball Bat", 0.7f),
-            new ItemSpawnInfo("Item Melee Frying Pan", 0.6f),
-            new ItemSpawnInfo("Item Melee Sledge Hammer", 0.5f),
-            new ItemSpawnInfo("Item Melee Sword", 0.5f)
-        };
-
         static void Postfix(ShopManager __instance)
         {
             System.Random rng = new System.Random();
 
             foreach (var item in StatsManager.instance.itemDictionary.Values)
             {
-                var itemInfo = itemSpawnInfos.FirstOrDefault(info => info.ItemName == item.itemAssetName);
-                
+                // Verificar la probabilidad del ítem usando la lista configurada
+                var itemInfo = Plugin.ItemSpawnInfos.FirstOrDefault(info => info.ItemName == item.itemAssetName);
+
                 if (itemInfo != null)
                 {
                     Plugin.Logger.LogInfo($"Evaluando aparición de {item.itemAssetName}...");
@@ -57,51 +25,6 @@ namespace REPO_Rebalanced
                         __instance.potentialItems.Add(item);
                     }
                 }
-            }
-        }
-    }
-
-    [HarmonyPatch(typeof(ChatManager), "OnChatMessageReceived")]
-    public class ChatManagerPatch
-    {
-        static void Postfix(string message)
-        {
-            if (message.StartsWith("/setprobability"))
-            {
-                string[] args = message.Split(' ');
-
-                if (args.Length == 3)
-                {
-                    string itemName = args[1];
-                    if (float.TryParse(args[2], out float newProbability) && newProbability >= 0 && newProbability <= 1)
-                    {
-                        ModifyItemProbability(itemName, newProbability);
-                    }
-                    else
-                    {
-                        Plugin.Logger.LogInfo("La probabilidad debe estar entre 0 y 1.");
-                    }
-                }
-                else
-                {
-                    Plugin.Logger.LogInfo("Uso correcto: /setprobability <itemName> <probabilidad>");
-                }
-            }
-        }
-
-        private static void ModifyItemProbability(string itemName, float newProbability)
-        {
-            // Acceder a la lista a través de ShopManagerPatch
-            var itemSpawnInfo = ShopManagerPatch.itemSpawnInfos.FirstOrDefault(info => info.ItemName == itemName);
-
-            if (itemSpawnInfo != null)
-            {
-                itemSpawnInfo.SpawnChance = newProbability;
-                Plugin.Logger.LogInfo($"Probabilidad de {itemName} actualizada a {newProbability * 100}%.");
-            }
-            else
-            {
-                Plugin.Logger.LogInfo($"Ítem {itemName} no encontrado.");
             }
         }
     }
